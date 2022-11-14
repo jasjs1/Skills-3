5fbf78ffcddad2e29f3cb43988583f7da1f75529
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2022-11-13 19:55:38 -0600

    MVP

---
+++ Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Skills 3
+//
+//  Created by Jayce Sagvold on 11/13/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct Constants {
+    static let minute: Int = 60
+    static let hour: Int = 3600
+    static let day: Int = 86400
+}
